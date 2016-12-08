300091f76cbdafee44a8f000f1486998b9f13a1c
Ye Yin <eyniy@qq.com>
2016-12-08 20:24:08 +0800

    Update for rbd

--- lxcfs.c
+++ lxcfs.c
@@ -1732,42 +1732,46 @@ int proc_diskstats_read(char *buf, size_t size, off_t offset,
 		//i = sscanf(line, "%u %u %s %*lu %*lu %*lu %*lu %*lu %*lu %*lu %*lu %*lu %lu", 
 		i = sscanf(line, "%u %u %s", &major, &minor, dev_name);
 		if(i == 3){
-			get_blkio_io_value(io_serviced_str, major, minor, "Read", &read);
-			get_blkio_io_value(io_serviced_str, major, minor, "Write", &write);
-			get_blkio_io_value(io_merged_str, major, minor, "Read", &read_merged);
-			get_blkio_io_value(io_merged_str, major, minor, "Write", &write_merged);
-			get_blkio_io_value(io_service_bytes_str, major, minor, "Read", &read_sectors);
-			read_sectors = read_sectors/512;
-			get_blkio_io_value(io_service_bytes_str, major, minor, "Write", &write_sectors);
-			write_sectors = write_sectors/512;
-			
-			get_blkio_io_value(io_service_time_str, major, minor, "Read", &rd_svctm);
-			rd_svctm = rd_svctm/1000000;
-			get_blkio_io_value(io_wait_time_str, major, minor, "Read", &rd_wait);
-			rd_wait = rd_wait/1000000;
-			read_ticks = rd_svctm + rd_wait;
+            if (strncmp (dev_name, "rbd", 3) == 0) {
+                printme = line;
+            } else {
+			    get_blkio_io_value(io_serviced_str, major, minor, "Read", &read);
+			    get_blkio_io_value(io_serviced_str, major, minor, "Write", &write);
+			    get_blkio_io_value(io_merged_str, major, minor, "Read", &read_merged);
+			    get_blkio_io_value(io_merged_str, major, minor, "Write", &write_merged);
+			    get_blkio_io_value(io_service_bytes_str, major, minor, "Read", &read_sectors);
+                read_sectors = read_sectors/512;
+			    get_blkio_io_value(io_service_bytes_str, major, minor, "Write", &write_sectors);
+                write_sectors = write_sectors/512;
+			    
+			    get_blkio_io_value(io_service_time_str, major, minor, "Read", &rd_svctm);
+			    rd_svctm = rd_svctm/1000000;
+			    get_blkio_io_value(io_wait_time_str, major, minor, "Read", &rd_wait);
+			    rd_wait = rd_wait/1000000;
+			    read_ticks = rd_svctm + rd_wait;
 
-			get_blkio_io_value(io_service_time_str, major, minor, "Write", &wr_svctm);
-			wr_svctm =  wr_svctm/1000000;
-			get_blkio_io_value(io_wait_time_str, major, minor, "Write", &wr_wait);
-			wr_wait =  wr_wait/1000000;
-			write_ticks = wr_svctm + wr_wait;
+			    get_blkio_io_value(io_service_time_str, major, minor, "Write", &wr_svctm);
+			    wr_svctm =  wr_svctm/1000000;
+			    get_blkio_io_value(io_wait_time_str, major, minor, "Write", &wr_wait);
+			    wr_wait =  wr_wait/1000000;
+			    write_ticks = wr_svctm + wr_wait;
 
-			/*get_blkio_io_value(io_service_time_str, major, minor, "Total", &tot_ticks);
-			tot_ticks =  tot_ticks/1000000;*/
-			get_blkio_value(io_time_str, major, minor, &tot_ticks);	
+			    /*get_blkio_io_value(io_service_time_str, major, minor, "Total", &tot_ticks);
+			    tot_ticks =  tot_ticks/1000000;*/
+			    get_blkio_value(io_time_str, major, minor, &tot_ticks);
+		        if (read || write || read_merged || write_merged || read_sectors || write_sectors || read_ticks || write_ticks) {
+		            memset(lbuf, 0, 256);
+			        snprintf(lbuf, 256, "%u       %u %s %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu\n", 
+				        major, minor, dev_name, read, read_merged, read_sectors, read_ticks,
+				        write, write_merged, write_sectors, write_ticks, ios_pgr, tot_ticks, rq_ticks);
+			        printme = lbuf;
+		        } else {
+			        continue;
+                }
+            }
 		}else{
 			continue;
 		}
-
-		memset(lbuf, 0, 256);
-		if (read || write || read_merged || write_merged || read_sectors || write_sectors || read_ticks || write_ticks) {
-			snprintf(lbuf, 256, "%u       %u %s %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu\n", 
-				major, minor, dev_name, read, read_merged, read_sectors, read_ticks,
-				write, write_merged, write_sectors, write_ticks, ios_pgr, tot_ticks, rq_ticks);
-			printme = lbuf;
-		} else
-			continue;
 
 		l = snprintf(buf, size, "%s", printme);
 		buf += l;
--- setns.h
+++ setns.h
@@ -2,10 +2,18 @@
 #if __GLIBC__ == 2 && __GLIBC_MINOR__ < 14
 #include <sched.h>
 #include "syscall.h"
+#define _GNU_SOURCE
+#include <sched.h>
+#include "syscall.h"
 #ifdef SYS_setns
 int setns(int fd, int nstype)
 {
-        return syscall(SYS_setns, fd, nstype);
+    return syscall(__SYS_setns, fd, nstype);
+}
+#elif __NR_setns
+int setns(int fd, int nstype)
+{
+    return syscall(__NR_setns, fd, nstype);
 }
 #endif
 #endif
